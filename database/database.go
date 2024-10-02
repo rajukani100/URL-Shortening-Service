62cc66bee6c97f440aa900ee6dc91bdf59efddff
Raj Ukani <rajukani100@gmail.com>
2024-10-02 12:56:03 +0530

    change var name

--- database/database.go
+++ database/database.go
@@ -25,6 +25,6 @@ func ConnectMongoDB() {
 	fmt.Println("Connected to MongoDB.")
 }
 
-func getUrlsCollection() *mongo.Collection {
+func GetUrlsCollection() *mongo.Collection {
 	return MongoClient.Database("URL-Shortening-Service").Collection("URLs")
 }
