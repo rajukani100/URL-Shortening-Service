295d74240955a167f006db29e46a44e44cc39fb2
Raj Ukani <rajukani100@gmail.com>
2024-10-02 19:54:31 +0530

    improved structure

--- models/urlModel.go
+++ models/urlModel.go
@@ -11,10 +11,22 @@ type URL struct {
 }
 
 type URL_INFO struct {
+	Url       string    `bson:"url" json:"url"`
+	ShortCode string    `bson:"shorten" json:"shorten"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+type Stats struct {
+	Id       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Views    int                `bson:"views" json:"views"`
+	Url_info URL_INFO
+}
+
+type ShortenResponse struct {
 	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Url       string             `bson:"url" json:"url"`
 	ShortCode string             `bson:"shorten" json:"shorten"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
-	Views     int                `bson:"views" json:"views"`
 }
