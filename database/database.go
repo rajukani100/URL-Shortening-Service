ff2954d14df7db2d0adb1af34424b6a37801657d
Raj Ukani <rajukani100@gmail.com>
2024-10-02 11:46:58 +0530

    added getConnection method

--- database/database.go
+++ database/database.go
@@ -1,1 +1,19 @@
 package database
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var mongoClient *mongo.Client
+
+func getConnection() (*mongo.Client, error) {
+	mongoClient, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/"))
+	if err != nil {
+		return nil, err
+	}
+	return mongoClient, nil
+
+}
