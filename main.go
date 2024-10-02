432f3fb3106632543c33616fe477c2e688ee2856
Raj Ukani <rajukani100@gmail.com>
2024-10-02 12:15:26 +0530

    added shorten handler

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"UrlShorteningService/controllers"
+	"UrlShorteningService/database"
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	fmt.Println("Url Shorterning service started....")
+
+	//connect Mongo
+	database.ConnectMongoDB()
+
+	//handlers
+	http.HandleFunc("POST /shorten", controllers.CreateShorten)
 
 }
